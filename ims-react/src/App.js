546ad093f5d3608dc8aba927bcceafd859756280
zonic-ultra <zonicultra@gmail.com>
2025-06-30 21:32:05 +0700

    show login page on app start

--- ims-react/src/App.js
+++ ims-react/src/App.js
@@ -10,7 +10,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/register" element={<RegisterPage />} />
-        <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={<LoginPage />} />
 
         {/* ADMIN ROUTES */}
         <Route
